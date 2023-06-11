b37ac8ce0d569b040944598c183eb68e1d8260cd
Jorge Netto <jorgenetto96@gmail.com>
2023-06-11 11:56:15 -0300

    Working calculator - Adds operators functionality and equals functionality
    - when the user clicks one of the operators, it updates the 'firstNumber' value.
    - when the user clicks the 'equals', it updates the secondNumber and calls the 'operate'
    function. Then, it update the display Value with the operation value.

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ let operator = null;
 // // ### 3. Create a function 'operate' that takes one operator and two numbers and calls one of the functions created earlier.
 
 // how this function works: you write one of the operations (add,subtract..etc) and two numbers. The function will use it as a callback function, passing the arguments you used.
-function operate(operator, numberA, numberB) {
+export function operate(operator, numberA, numberB) {
     return operator(numberA, numberB);
 };
 
--- assets/ui.js
+++ assets/ui.js
@@ -1,6 +1,6 @@
 // Import the math operations functions
 import { add, subtract, multiply, divide } from './mathOperations.js';
-
+import { operate } from './script.js';
 // gets the display div.
 let display = document.querySelector('#displayValue');
 
@@ -13,10 +13,14 @@ const btnBackspace = document.querySelector('#btnBackspace');
 // clear button
 const btnClear = document.querySelector('#btnClear');
 
-// ### 5. Functions that populate the display when the numbers are clicked.
+// get the math ops buttons
+const btnAdd = document.querySelector('#btnAdd');
+const btnSubtract = document.querySelector('#btnSubtract');
+const btnMultiply = document.querySelector('#btnMultiply');
+const btnDivide = document.querySelector('#btnDivide');
 
 // ## 0-9 numbers click functionality
-// # <
+
 
 function regularNumbers(e) {
     // verifications:
@@ -32,10 +36,10 @@ function regularNumbers(e) {
         display.innerText += e.target.textContent; 
     };
 }
-// # </
+
 
 // ## AC button functionality 
-// # <
+
 
 
 // When the 'AC' button is clicked, the display value becomes zero '0';
@@ -44,10 +48,10 @@ function clear(e) {
         display.innerText = 0;
     }
 };
-// # />
+
 
 // ## Backspace functionality 
-// # <
+
 
 function backspace() {
 
@@ -58,11 +62,67 @@ function backspace() {
     // atualiza o display para que ele seja o novo número.
     display.innerText = str;
 };
-// # />
+
+// ## Math Ops Functionalities 
+
+// Add
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
+btnAdd.addEventListener('click', (e) => {
+    if (firstNumber == null) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerText = 0;
+    }
+    operator = add;
+    // console.log(display.innerText);
 
 
+});
 
-// Functionalities
+btnSubtract.addEventListener('click', (e) => {
+    if (firstNumber == null) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerText = 0;
+    }
+    console.log('subtract');
+    return operator = subtract;
+});
+
+btnMultiply.addEventListener('click', (e) => {
+    if (firstNumber == null) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerText = 0;
+    }
+    console.log('multiply');
+    return operator = multiply;
+});
+
+btnDivide.addEventListener('click', (e) => {
+    if (firstNumber == null) {
+        firstNumber = parseFloat(display.innerText);
+        display.innerText = 0;
+    }
+    console.log('divide');
+    return operator = divide;
+});
+
+btnEquals.addEventListener('click', (e) => {
+    if (secondNumber == null) {
+        secondNumber = parseFloat(display.innerText);
+    }
+    console.log(`the first number is ${firstNumber}.`)
+    console.log(`the second number is: ${secondNumber}.`);
+    console.log(`the full operation is: = ${firstNumber} (+,-,*,/) ${secondNumber}`)
+    let result = operate(operator, firstNumber, secondNumber);
+    console.log(`the result is: ${result}`);
+    display.innerText = result;
+} )
+
+
+// Event Listeners
 
 // Event listener for '0-9 number' buttons;
 buttons.addEventListener('click', regularNumbers);
